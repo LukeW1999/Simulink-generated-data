2f540d239260bf3d2a3cd4df85dc7f6d58216b24
LukeW1999 <lukewang19@outlook.com>
2024-10-04 13:51:10 +0100

    changed to ESBMC_assert

--- example_as/main.c
+++ example_as/main.c
@@ -11,15 +11,15 @@ int main() {
     int32_t totalLenRd_old = pIp.totalLenRd;
 
     // Preconditions
-    __CPROVER_assume(pIp.rdLen != 19);
+    __ESBMC_assume(pIp.rdLen != 19);
 
     // Call the IP interface to be verified
     Fg333saCheckFun(&pIp);
 
     // Postconditions
-    __CPROVER_assert(pIp.bComSuc == 0,"");
-    __CPROVER_assert(pIp.cntLenRd == cntLenRd_old + 1,"");
-    __CPROVER_assert(pIp.totalLenRd == totalLenRd_old + 1,"");
+    __ESBMC_assert(pIp.bComSuc == 0,"");
+    __ESBMC_assert(pIp.cntLenRd == cntLenRd_old + 1,"");
+    __ESBMC_assert(pIp.totalLenRd == totalLenRd_old + 1,"");
 
     return 0;
 }
